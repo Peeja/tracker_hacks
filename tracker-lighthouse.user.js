8b1f7506e3d6cd5a389d5ab463a12ed229407092
Peter Jaros <peeja@dropio.com>
2009-06-10 12:03:17 -0400

    Restore Lighthouse settings to fields on load.

--- tracker-lighthouse.user.js
+++ tracker-lighthouse.user.js
@@ -9,18 +9,36 @@ var trackerCode = function() {
 }
 
 var settingsCode = function() {
-  var section = <html><![CDATA[
+  function LighthouseSettings(settings) {
+    if (settings)
+      createCookie("lighthouseSettings", $H(settings).toJSON());
+    else
+      return $H(readCookie("lighthouseSettings").evalJSON(true));
+  };
+  
+  $$(".content_section")[2].insert({after: <html><![CDATA[
     <div id="lighthouse_settings" class="content_section">
       <h2>Lighthouse</h2>
       <table class="form_table">
-        <tr><td class="label_column"><label for="project_lighthouse_account">Lighthouse Account</label></td><td class="field_column"><input id="project_lighthouse_account" name="project[lighthouse_account]" type="input" />.lighthouseapp.com</td></tr>
-        <tr><td class="label_column"><label for="project_lighthouse_project_id">Project ID</label></td><td class="field_column"><input id="project_lighthouse_project_id" class="short_field" name="project[lighthouse_project_id]" type="input" size="3" /></td></tr>
-        <tr><td class="label_column"><label for="project_lighthouse_api_token">Lighthouse API Token</label></td><td class="field_column"><input id="project_lighthouse_api_token" name="project[lighthouse_api_token]" type="input" /></td></tr>
+        <tr>
+          <td class="label_column"><label for="project_lighthouse_account">Lighthouse Account</label></td>
+          <td class="field_column"><input id="project_lighthouse_account" name="project[lighthouse_account]" type="input" />.lighthouseapp.com</td>
+        </tr>
+        <tr>
+          <td class="label_column"><label for="project_lighthouse_project_id">Project ID</label></td>
+          <td class="field_column"><input id="project_lighthouse_project_id" class="short_field" name="project[lighthouse_project_id]" type="input" size="3" /></td>
+        </tr>
+        <tr>
+          <td class="label_column"><label for="project_lighthouse_api_token">Lighthouse API Token</label></td>
+          <td class="field_column"><input id="project_lighthouse_api_token" name="project[lighthouse_api_token]" type="input" /></td>
+        </tr>
       </table>
     </div>
-  ]]></html>.toString();
+  ]]></html>.toString()});
   
-  $$(".content_section")[2].insert({after: section});
+  LighthouseSettings().each(function(pair) {
+    $("project_"+pair.key).value = pair.value;
+  });
   
   // On submit, save the Lighthouse settings in a cookie, then remove the
   // fields and resubmit so that we don't send the info to Tracker.
@@ -29,10 +47,12 @@ var settingsCode = function() {
       e.stop();
     
       var settings = this.serialize(true);
-      createCookie("lighthouseSettings", [settings["project[lighthouse_account]"],
-                                          settings["project[lighthouse_project_id]"],
-                                          settings["project[lighthouse_api_token]"]].join("|"));
-    
+      
+      LighthouseSettings($w("lighthouse_account lighthouse_project_id lighthouse_api_token").inject({}, function(h, setting) {
+        h[setting] = settings["project["+setting+"]"];
+        return h;
+      }));
+      
       $("lighthouse_settings").remove();
     
       this.submit();
